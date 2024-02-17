ce8750b4f6fa1d3417ef9043f72dac9f4eeb68fc
kazimlaos <1LAOSK42@solent.ac.uk>
2024-02-17 17:33:43 +0000

    adding the project structure and the initScript

---
+++ initScript.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+jupyter notebook --allow-root --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
+#voila --no-browser --template vuetify-default --enable_nbextensions=True frontend/Interface.ipynb 
+--Voila.ip=0.0.0.0 --port 8888
