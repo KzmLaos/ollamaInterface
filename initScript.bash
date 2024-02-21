1982f06662fe01c63dcaf09134fb8dabdbdd2a0a
kazimlaos <1LAOSK42@solent.ac.uk>
2024-02-21 23:17:24 +0000

    updating script

--- initScript.bash
+++ initScript.bash
@@ -5,7 +5,8 @@ then
 	jupyter notebook --allow-root --no-browser --port 8888 --ip=0.0.0.0 --NotebookApp.token=''
 elif [ "$A" == "deploy" ];
 then
-	voila --no-browser --template vuetify-default --enable_nbextensions=True frontend/Interface.ipynb --Voila.ip=0.0.0.0 --port 8888
+	voila --no-browser --template vuetify-default --enable_nbextensions=True 
+/home/debian/ollamainterface/frontend/Interface.ipynb --Voila.ip=0.0.0.0 --port 8888
 else
 	echo "unknown input"
 fi
